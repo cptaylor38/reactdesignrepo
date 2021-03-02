8e655b7bdb7be74d55c54eac02b267a7c7899d36
Corbin Taylor <cptaylor38@yahoo.com>
2021-03-02 01:47:14 -0500

    added more to space and video for another idea

--- designplayground/src/Pages/Space/Space.jsx
+++ designplayground/src/Pages/Space/Space.jsx
@@ -8,17 +8,28 @@ const Space = () => {
         <div className='spaceBody'>
             <nav className='navbar'>
                 <p>Logo</p>
-                <p>Link</p>
-                <p>Link</p>
-                <p>Link</p>
+                <div>
+                    <p>Link</p>
+                    <p>Link</p>
+                    <p>Link</p>
+                </div>
             </nav>
             <Container className='spaceMain'>
                 <main>
-                    Space
+                    Stellar
                 </main>
                 <video loop autoPlay muted>
                     <source src={video} type='video/mp4'/>
                 </video>
+            </Container>
+            <Container className='spaceSub'>
+                <h1>Some info text</h1>
+                <section>
+                    <article>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aspernatur molestiae tempore necessitatibus nisi sed eius, error officia ipsam exercitationem saepe velit, autem, nobis quia sunt odio repellendus. Blanditiis, architecto!</article>
+                    <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos adipisci in temporibus eos tenetur numquam cum est, non fuga, qui architecto voluptas. Corporis magnam dignissimos soluta esse assumenda veritatis consectetur.</article>
+                    <article>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aspernatur molestiae tempore necessitatibus nisi sed eius, error officia ipsam exercitationem saepe velit, autem, nobis quia sunt odio repellendus. Blanditiis, architecto!</article>
+                    <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos adipisci in temporibus eos tenetur numquam cum est, non fuga, qui architecto voluptas. Corporis magnam dignissimos soluta esse assumenda veritatis consectetur.</article> 
+                </section>
             </Container>
         </div>
     )
