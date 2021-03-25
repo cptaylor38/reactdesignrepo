1a93a1c2241ca356c1c4e0bc25d2552585ec3b16
Corbin Taylor <cptaylor38@yahoo.com>
2021-03-24 23:43:17 -0400

    trying out idea for drug manufacturuer

--- designplayground/src/Pages/Wellzal/Wellzal.js
+++ designplayground/src/Pages/Wellzal/Wellzal.js
@@ -5,10 +5,13 @@ import video from '../../Assets/Images/flowerfield.mp4';
 
 const Wellzal = () => {
   return (
-    <Container fluid id='wel__page__container'>
-      <video autoPlay id='wel__video'>
+    <Container fluid className='wel__page__container'>
+      <video autoPlay className='wel__video'>
         <source src={video} type='video/mp4'></source>
       </video>
+      <div className='wel__lead'>
+        <h1>Ready to feel better?</h1>
+      </div>
     </Container>
   );
 };
