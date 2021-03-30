e9179e9c16e4a9f03b05b424212c914cd0ae81e6
Corbin Taylor <cptaylor38@yahoo.com>
2021-03-30 00:17:29 -0400

    playing around with gsap and learning more about color theory - changing landing

--- designplayground/src/Pages/Wellzal/Components/Hero/Hero.js
+++ designplayground/src/Pages/Wellzal/Components/Hero/Hero.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Hero.scss';
-import wel_image from '../../../../Assets/Images/wellzal.jpg';
+import wel_image from '../../../../Assets/Images/bubbles.jpg';
+import { TimelineMax, TweenMax, Power4 } from 'gsap';
 
 const Hero = () => {
+  const heroImage = useRef(null);
+  let t1 = new TimelineMax();
+  useEffect(() => {
+    t1.fromTo(
+      heroImage.current,
+      { maxHeight: '100vh' },
+      { maxHeight: '50vh', duration: 2, ease: Power4.easeOut }
+    );
+  }, []);
   return (
     <div className='wel__v__container'>
       <img
+        ref={heroImage}
         src={wel_image}
-        alt='wellzal stock photo'
+        alt='unsplash bubbles'
         className='wel__hero__image'
       />
       <div className='wel__hero_description'>
--- designplayground/src/Pages/Wellzal/Wellzal.js
+++ designplayground/src/Pages/Wellzal/Wellzal.js
@@ -4,10 +4,12 @@ import { Route } from 'react-router-dom';
 import { Container } from 'reactstrap';
 import Landing from './Pages/Landing/Landing';
 import Assistance from './Pages/Assistance/Assistance';
+import Nav from './Components/Nav/Nav';
 
 const Wellzal = () => {
   return (
     <Container fluid className='wel__page__container'>
+      <Nav />
       <Route exact path='/wellzal' component={Landing} />
       <Route path='wellzal/assistance' component={Assistance} />
     </Container>
